dd06ec37c572aed96d450065e4f615ad5587088f
vaquierm <michaelvaquier74@gmail.com>
2020-02-05 16:39:41 -0500

    Removed all unecessary files

--- _build/exceptions.ml
+++
@@ -1,5 +0,0 @@
-exception LexerError of string
-(* Messge, line number, char numbers *)
-exception UnsuportedError of string * int * ((int * int) option)
-(* Messge, line number *)
-exception SyntaxError of string * (int option)
